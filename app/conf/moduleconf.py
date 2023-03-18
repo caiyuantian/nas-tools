82543cbdf1b4db81efb7f86e562ec1ab38f30b62
robin cai <caiyuantian@gmail.com>
2023-03-18 21:42:29 +0800

    Add back navbar, indexer and downloader

--- app/conf/moduleconf.py
+++ app/conf/moduleconf.py
@@ -611,6 +611,35 @@ class ModuleConf(object):
                     "placeholder": "password"
                 }
             }
+        },
+        "aria2": {
+            "name": "Aria2",
+            "img_url": "../static/img/downloader/aria2.png",
+            "monitor_enable": True,
+            "speedlimit_enable": True,
+            "config": {
+                "host": {
+                    "id": "aria2_host",
+                    "required": True,
+                    "title": "Aria2地址",
+                    "type": "text",
+                    "placeholder": "127.0.0.1"
+                },
+                "port": {
+                    "id": "aria2_port",
+                    "required": True,
+                    "title": "Aria2 RPC端口",
+                    "type": "text",
+                    "placeholder": "6800"
+                },
+                "secret": {
+                    "id": "aria2_secret",
+                    "required": False,
+                    "title": "密码令牌",
+                    "type": "text",
+                    "placeholder": ""
+                }
+            }
         }
     }
 
@@ -713,7 +742,64 @@ class ModuleConf(object):
     }
 
     # 索引器
-    INDEXER_CONF = {}
+    INDEXER_CONF = {
+        "jackett": {
+            "name": "Jackett",
+            "img_url": "./static/img/indexer/jackett.png",
+            "background": "bg-black",
+            "test_command": "app.indexer.client.jackett|Jackett",
+            "config": {
+                "host": {
+                    "id": "jackett.host",
+                    "required": True,
+                    "title": "Jackett地址",
+                    "tooltip": "Jackett访问地址和端口，如为https需加https://前缀。注意需要先在Jackett中添加indexer，才能正常测试通过和使用",
+                    "type": "text",
+                    "placeholder": "http://127.0.0.1:9117"
+                },
+                "api_key": {
+                    "id": "jackett.api_key",
+                    "required": True,
+                    "title": "Api Key",
+                    "tooltip": "Jackett管理界面右上角复制API Key",
+                    "type": "text",
+                    "placeholder": ""
+                },
+                "password": {
+                    "id": "jackett.password",
+                    "required": False,
+                    "title": "密码",
+                    "tooltip": "Jackett管理界面中配置的Admin password，如未配置可为空",
+                    "type": "password",
+                    "placeholder": ""
+                }
+            }
+        },
+        "prowlarr": {
+            "name": "Prowlarr",
+            "img_url": "../static/img/indexer/prowlarr.png",
+            "background": "bg-orange",
+            "test_command": "app.indexer.client.prowlarr|Prowlarr",
+            "config": {
+                "host": {
+                    "id": "prowlarr.host",
+                    "required": True,
+                    "title": "Prowlarr地址",
+                    "tooltip": "Prowlarr访问地址和端口，如为https需加https://前缀。注意需要先在Prowlarr中添加搜刮器，同时勾选所有搜刮器后搜索一次，才能正常测试通过和使用",
+                    "type": "text",
+                    "placeholder": "http://127.0.0.1:9696"
+                },
+                "api_key": {
+                    "id": "prowlarr.api_key",
+                    "required": True,
+                    "title": "Api Key",
+                    "tooltip": "在Prowlarr->Settings->General->Security-> API Key中获取",
+                    "type": "text",
+                    "placeholder": ""
+                }
+            }
+        }
+    }
 
     # 发现过滤器
     DISCOVER_FILTER_CONF = {
--- app/downloader/client/__init__.py
+++ app/downloader/client/__init__.py
@@ -1,2 +1,5 @@
 from .qbittorrent import Qbittorrent
 from .transmission import Transmission
+from .aria2 import Aria2
+from .client115 import Client115
+from .pikpak import PikPak
